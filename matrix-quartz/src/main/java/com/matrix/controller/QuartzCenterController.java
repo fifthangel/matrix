04c7b0d3b33174654c22f23aa1c32cb2ced6ba51
yangchenglin <chenglin.yang@dingding.com>
2017-11-02 16:54:10 +0800

    定时任务列表封装中

---
+++ matrix-quartz/src/main/java/com/matrix/controller/QuartzCenterController.java
@@ -0,0 +1,57 @@
+package com.matrix.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @description: 定时任务统一封装与处理 
+ *
+ * @author Yangcl
+ * @date 2017年11月2日 下午4:52:31 
+ * @version 1.0.0
+ */
+@Controller
+@RequestMapping("quartz")
+public class QuartzCenterController {
+	private static Logger logger=Logger.getLogger(QuartzCenterController.class);
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
