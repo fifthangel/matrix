992f4a252af73b6ee8f0ae2ad333b51b1c72fb6f
Yangcl <794867067@qq.com>
2018-09-06 23:41:21 +0800

    开始完善高可用分布式锁

---
+++ matrix-cache/src/main/java/com/matrix/support/ZkLockPathEnum.java
@@ -0,0 +1,17 @@
+package com.matrix.support;
+
+/**
+ * @description: 
+ *
+ * @author Yangcl
+ * @home https://github.com/PowerYangcl
+ * @date 2018年9月6日 下午11:34:54 
+ * @version 1.0.0.1
+ */
+public enum ZkLockPathEnum {
+
+	zkLockRoot,        // zookeeper锁基础路径
+	
+	
+	
+}
