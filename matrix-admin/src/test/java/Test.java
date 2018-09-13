e3dcb6793cb2fb7df1f3579a28240b5333f32981
Yangcl <794867067@qq.com>
2018-09-13 21:13:27 +0800

    2

---
+++ matrix-admin/src/test/java/Test.java
@@ -0,0 +1,21 @@
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import com.matrix.util.NetUtil;
+
+import java.util.Date;
+import java.util.Properties;
+
+/**
+ * JavaMail 版本: 1.6.0
+ * JDK 版本: JDK 1.7 以上（必须）
+ */
+public class Test {
+
+	
+	
+	
+}
